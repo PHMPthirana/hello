6b7740ed09c37c3edf341d26a46ef40384063348
Harshila <93441894+PHMPthirana@users.noreply.github.com>
2021-10-31 16:29:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,11 @@ bot = telebot.TeleBot("2059677624:AAEiHUtgR6VYNStUdpn1F3UaIpJQTseHmP0")
 def send_welcome(message):
   bot.reply_to(message, "Hello! I'm Hatrshila Chat Bot")
 
-bot = telebot.TeleBot("2059677624:AAEiHUtgR6VYNStUdpn1F3UaIpJQTseHmP0")
-  
+
 @bot.message_handler(commands=["how"])
 def send_message(message):
   bot.send_message(message, "How")
   
-bot = telebot.TeleBot("2059677624:AAEiHUtgR6VYNStUdpn1F3UaIpJQTseHmP0")  
   
 @bot.message_handler(commands=["hello"])
 def send_message(message):
