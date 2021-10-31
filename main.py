8c32632304fa8a498943b12fd67aed12bfded728
Harshila <93441894+PHMPthirana@users.noreply.github.com>
2021-10-31 11:46:40 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+import telebot
+
+
+bot = telebot.TeleBot("059677624:AAGUQpY8_WrRtc8aTwOoR_j4NJUsP8wh3rE")
+
+
+@bot.message_handler(commands=["start"])
+def send_welcome(message):
+  bot.reply_to(message, "Hello! I'm Hatrshila Chat Bot")
+
+
+@bot.message_handler(commands=["how"])
+def send_message(message):
+  bot.send_message(message.chat.id, "How")
+
+
+bot.polling()
