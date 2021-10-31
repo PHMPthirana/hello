8c30ed23b39502f6c99025099f1d9f7d5dfec7af
Harshila <93441894+PHMPthirana@users.noreply.github.com>
2021-10-31 13:57:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import telebot
 
 
-bot = telebot.TeleBot("059677624:AAGUQpY8_WrRtc8aTwOoR_j4NJUsP8wh3rE")
+bot = telebot.TeleBot("2059677624:AAGUQpY8_WrRtc8aTwOoR_j4NJUsP8wh3rE")
 
 
 @bot.message_handler(commands=["start"])
@@ -12,7 +12,8 @@ def send_welcome(message):
 
 @bot.message_handler(commands=["how"])
 def send_message(message):
-  bot.send_message(message.chat.id, "How")
+  bot.send_message(message, "How")
 
 
 bot.polling()
+
